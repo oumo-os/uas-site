f1f51bdbead23c6f2bacbb85032da099a7cd41e1
oumo-os <samoumo@live.com>
2026-09-19 11:40:41 +0300

    Bump api.js to v9 (mail thread helpers)

--- admin/resolution-builder.php
+++ admin/resolution-builder.php
@@ -103,7 +103,7 @@
     <p>Uganda Astronomical Society &middot; Institutional Platform</p>
   </footer>
 
-  <script src="../js/api.js?v=20260911-8"></script>
+  <script src="../js/api.js?v=20260911-9"></script>
   <script>
     const memberOptions = [];
     const roleOptions = [];
--- member/submit-article.php
+++ member/submit-article.php
@@ -78,7 +78,7 @@
     <p>Uganda Astronomical Society &middot; Institutional Platform</p>
   </footer>
 
-  <script src="../js/api.js?v=20260911-8"></script>
+  <script src="../js/api.js?v=20260911-9"></script>
   <script>
     async function load() {
       try {
